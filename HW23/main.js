cefadc1cb697e6362ab91f1b654ffe34cd8a916e
antoninaklymchuk15 <klymchuk.antonina@gmail.com>
2024-03-07 21:24:11 +0200

    HW23 done

---
+++ HW23/main.js
@@ -0,0 +1,9 @@
+function showDiv() {
+  var taskDiv = document.getElementById("taskDiv");
+  taskDiv.style.display = "block";
+}
+
+function hideDiv() {
+  var taskDiv = document.getElementById("taskDiv");
+  taskDiv.style.display = "none";
+}
